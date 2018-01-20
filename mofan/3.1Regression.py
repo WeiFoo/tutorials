8e7d26baa9858b105bd0af025f9a43c9c3351021
WeiFoo <wfu@ncsu.edu>
2018-01-19 23:50:34 -0500

    regression done

--- mofan/3.1Regression.py
+++ mofan/3.1Regression.py
@@ -3,11 +3,15 @@ from torch.autograd import Variable
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
 
-x = torch.unsqueeze(torch.linspace(-1, -1, 100), dim=1)
-y = x.pow(2) + 0.2 * torch.rand(x.size())
+x = torch.unsqueeze(torch.linspace(-1, 1, 100), dim=1)
+y = x.pow(2) + 0.2*torch.rand(x.size())
 
 x, y = Variable(x), Variable(y)
 
+# plt.scatter(x.data.numpy(), y.data.numpy())
+# plt.show()
+
+#
 class Net(torch.nn.Module):
     def __init__(self, n_features, n_hidden, n_output):
         super(Net, self).__init__()
@@ -19,7 +23,8 @@ class Net(torch.nn.Module):
         x = self.predict(x)
         return x
 
-net = Net(1,10,1)
+
+net = Net(1, 10, 1)
 print net
 
 plt.ion()
@@ -41,10 +46,8 @@ for t in range(100):
         plt.cla()
         plt.scatter(x.data.numpy(), y.data.numpy())
         plt.plot(x.data.numpy(), prediction.data.numpy(), "r-", lw=5)
-        plt.text(0.5, 0, "Loss=%.4f" % loss.data[0], fontdict={"size":20,
-                                                              "color":"red"})
-        plt.pause(0.1)
+        plt.text(0.5, 0, "Loss=%.4f" % loss.data[0],
+                 fontdict={"size": 20, "color": "red"})
+        plt.pause(0.3)
 plt.ioff()
 plt.show()
-
-
